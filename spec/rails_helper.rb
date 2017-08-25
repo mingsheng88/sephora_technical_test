ca03fd3be6b4c3bd9d140f6e11bfb01a94a25989
Mingsheng Gan <mingsheng88@hotmail.com>
2017-08-25 17:35:11 +0800

    Remove unused gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,6 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'support/database_cleaner'
 require 'support/factory_girl'
-require 'support/shoulda_matchers'
 require 'support/json_helper'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
