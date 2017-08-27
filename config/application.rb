0192e65c028c2ccfa27bbf92d6d0f47453082071
Mingsheng Gan <mingsheng88@hotmail.com>
2017-08-27 23:21:02 +0800

    Setup jsonapi-resources
    Reference: https://github.com/cerebris/peeps

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module SephoraTechnicalTest
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.eager_load = true
+    config.consider_all_requests_local = false
+
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -12,7 +12,7 @@ describe Api::V1::ProductsController, type: :controller do
     it 'returns a specified product' do
       product, product_two = create_list(:product, 2)
       get :show, params: { id: product }
-      expect(json).to eq({ id: product.id })
+      expect(json[:id]).to eq(product.id)
     end
   end
 end
