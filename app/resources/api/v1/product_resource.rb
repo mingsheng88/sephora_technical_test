d7fd502e288d545579255bc870ee9edc832bf065
Mingsheng Gan <mingsheng88@hotmail.com>
2017-08-28 01:15:40 +0800

    Add filtering by price

--- app/resources/api/v1/product_resource.rb
+++ app/resources/api/v1/product_resource.rb
@@ -5,7 +5,13 @@ class Api::V1::ProductResource < JSONAPI::Resource
 
   filter :categories,
     default: '',
-    apply: ->(records, values, _options) {
-      records.in_categories(values)
-    }
+    apply: ->(records, values, _options) { records.in_categories(values) }
+
+  filter :price_from,
+    verify: ->(values, _context) { values.map { |value| BigDecimal(value) } },
+    apply: ->(records, values, _options) { records.price_range(from: values[0]) }
+
+  filter :price_to,
+    verify: ->(values, _context) { values.map { |value| BigDecimal(value) } },
+    apply: ->(records, values, _options) { records.price_range(to: values[0]) }
 end
--- spec/controllers/api/v1/product_controller_spec.rb
+++ spec/controllers/api/v1/product_controller_spec.rb
@@ -1,5 +1,5 @@
 describe Api::V1::ProductsController, type: :controller do
-  context 'GET api/v1/products' do
+  describe 'GET api/v1/products' do
     it 'returns all products' do
       product, product_two = create_list(:product, 2)
       get :index
@@ -7,7 +7,7 @@ describe Api::V1::ProductsController, type: :controller do
       expect(json.dig(:data, 1, :id)).to eq(product_two.id.to_s)
     end
 
-    describe 'categories' do
+    context 'categories' do
       it 'filters by categories' do
         product_one = create(:product, categories: ['cat', 'cat_one'])
         product_two = create(:product, categories: ['cat', 'cat_two'])
@@ -28,6 +28,33 @@ describe Api::V1::ProductsController, type: :controller do
         product_two = create(:product, categories: ['cat', 'cat_two'])
         get :index, params: { filter: { categories: 'NON_EXISTENT' } }
         expect(json.dig(:data).size).to eq(0)
+      end
+    end
+
+    context 'price' do
+      it 'filters by price_to' do
+        product_one = create(:product, price: 12)
+        product_two = create(:product, price: 15)
+        get :index, params: { filter: { price_to: 13 } }
+        expect(json.dig(:data).size).to eq(1)
+        expect(json.dig(:data, 0, :id)).to eq(product_one.id.to_s)
+      end
+
+      it 'filters by price_from' do
+        product_one = create(:product, price: 12)
+        product_two = create(:product, price: 15)
+        get :index, params: { filter: { price_from: 13 } }
+        expect(json.dig(:data).size).to eq(1)
+        expect(json.dig(:data, 0, :id)).to eq(product_two.id.to_s)
+      end
+
+      it 'filters by price_from and price_to' do
+        product_one = create(:product, price: 12)
+        product_two = create(:product, price: 15)
+        product_three = create(:product, price: 18)
+        get :index, params: { filter: { price_from: 13, price_to: 16 } }
+        expect(json.dig(:data).size).to eq(1)
+        expect(json.dig(:data, 0, :id)).to eq(product_two.id.to_s)
       end
     end
   end
