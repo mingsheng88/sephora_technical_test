25b0c2bcbecb1c43e31f0c81649bae10a05dd497
Mingsheng Gan <mingsheng88@hotmail.com>
2017-08-28 09:57:58 +0800

    Add fixme for sale implementation

--- db/migrate/20170825090158_create_products.rb
+++ db/migrate/20170825090158_create_products.rb
@@ -5,6 +5,12 @@ class CreateProducts < ActiveRecord::Migration[5.1]
       t.text :category, array: true, default: []
       t.integer :stock_status, default: 0, null: false
       t.monetize :price
+
+      # FIXME: For consideration, instead of these 3 fields, a better approach may be to:
+      # - separate model for sale
+      # - instead of sale price, go for value, with sale types, i.e. discount%, discount$ etc
+      # - instead of sale status, go for sale start date, sale end date
+      # - instead of sale text, generate text in helper class based on sale status
       t.monetize :sale_price
       t.integer :sale_status, default: 0, null: false
       t.string :sale_text
