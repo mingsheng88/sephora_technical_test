5e48e427c7e06fa1ee005bd2efb06204c1d8be79
Mingsheng Gan <mingsheng88@hotmail.com>
2017-08-31 16:40:36 +0800

    Remove products reference from root

--- app_frontend/components/root.vue
+++ app_frontend/components/root.vue
@@ -6,13 +6,11 @@
 </template>
 
 <script>
-  import Products from '../components/products.vue'
   import Header from '../components/header.vue'
 
   export default {
     name: 'root',
     components: {
-      'product': Products,
       'app-header': Header
     }
   }
