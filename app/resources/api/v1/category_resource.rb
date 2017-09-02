d6d75ac024f692f6a6dafc099a1ef04c3ee3e816
Mingsheng Gan <mingsheng88@hotmail.com>
2017-09-02 21:56:06 +0800

    Add slug to children

--- app/resources/api/v1/category_resource.rb
+++ app/resources/api/v1/category_resource.rb
@@ -1,9 +1,11 @@
 class Api::V1::CategoryResource < JSONAPI::Resource
-  attribute :name
+  attributes :name, :slug
   attribute :children
 
   def children
-    @model.children.map(&:name)
+    @model.children.map do |child|
+      { name: child.name, slug: child.slug }
+    end
   end
 
   def self.records(options = {})
--- spec/controllers/api/v1/categories_controller_spec.rb
+++ spec/controllers/api/v1/categories_controller_spec.rb
@@ -18,5 +18,14 @@ describe Api::V1::CategoriesController, type: :controller do
       get :index
       expect(json).to match_resources([root_category])
     end
+
+    it 'contains details about the children' do
+      root_category = create(:category)
+      child_category = create(:category, parent: root_category)
+      get :index
+      children = json.dig(:data, 0, :attributes, :children)
+      expect(children.size).to eq(1)
+      expect(children.first.keys).to include(:name, :slug)
+    end
   end
 end
