d210a30f8c866cfdcd28f8cb91cda42824445295
Mingsheng Gan <mingsheng88@hotmail.com>
2017-09-02 21:39:34 +0800

    Add slug to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,18 @@ class Category < ApplicationRecord
   has_ancestry
 
   has_and_belongs_to_many :products
+
+  before_save :generate_slug_before_save
+
+  validate :slug_not_changed
+
+  def slug_not_changed
+    if slug_changed?
+      errors.add(:slug, 'Slug can only be auto-generated!')
+    end
+  end
+
+  def generate_slug_before_save
+    self.slug = name.parameterize
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,25 @@
-require 'rails_helper'
+describe Category, type: :model do
+  describe 'slug' do
+    it 'generates slug on creation' do
+      category = described_class.create(name: 'A Category Name')
+      expect(category.slug).to eq('a-category-name')
+    end
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it 'generates slug on update' do
+      category = described_class.create(name: 'A Category Name')
+      category.update(name: 'New Category Name')
+      expect(category.slug).to eq('new-category-name')
+    end
+
+    it 'fails validation if modified' do
+      category = described_class.create(name: 'something', slug: 'something else')
+      expect(category).to be_invalid
+    end
+
+    it 'fails validation if modified' do
+      category = described_class.create(name: 'A Category Name')
+      category.update(name: 'Something', slug: 'something else')
+      expect(category).to be_invalid
+    end
+  end
 end
